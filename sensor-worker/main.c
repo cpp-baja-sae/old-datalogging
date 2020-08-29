fc2ec64ee7ae272990e61b0b34138141c9bccd0f
bajapi <bajapi@raspberrypi.local>
2020-08-28 20:40:40 -0700

    Extracted realtime stream worker.

--- sensor-worker/main.c
+++ sensor-worker/main.c
@@ -16,10 +16,7 @@
 #include "workers.h"
 
 FILE *primary_file;
-char primary_file_buffer[FILE_BUFFER_SIZE];
 int pbuf_read_index = 0;
-int pbuf_stream_index = 0;
-int pbuf_write_index = 0;
 
 struct LodInfo {
     int min_buffer[FRAME_LEN];
@@ -74,7 +71,7 @@ void begin_data_log_file() {
 
 // chunk_size must be a factor of FILE_BUFFER_SIZE
 void write_new_data_to_file(
-    char *source, FILE *target, int *read_index, int write_index, int chunk_size 
+    volatile char *source, FILE *target, int *read_index, int write_index, int chunk_size 
 ) {
     int unwritten_bytes;
     if (*read_index == write_index) {
@@ -94,7 +91,10 @@ void write_new_data_to_file(
     while (unwritten_bytes > chunk_size) {
         // We can safely assume the data does not wrap around because the
         // size of the whole buffer is a multiple of chunk_size
+        #pragma GCC diagnostic push
+        #pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
         fwrite(&source[*read_index], 1, chunk_size, target);
+        #pragma GCC diagnostic pop
         unwritten_bytes -= chunk_size;
         *read_index = (*read_index + chunk_size) % FILE_BUFFER_SIZE;
     }
@@ -126,7 +126,7 @@ void *file_worker(void *args) {
             begin_data_log_file();
             // Skip to the most currently written frame.
             pbuf_read_index = pbuf_write_index;
-            pbuf_read_index -= pbuf_stream_index % FRAME_SIZE;
+            pbuf_read_index -= pbuf_write_index % FRAME_SIZE;
             while (continue_flag && write_to_file_flag) {
                 update_files();
                 usleep(4000);
@@ -248,97 +248,6 @@ void *sensor_read_worker(void *args) {
     sensor_read_loop();
 
     gpioTerminate();
-}
-
-int send_unstreamed_data(int client_fd) {
-    int sample_interval = stream_frame_interval * FRAME_SIZE;
-    int unwritten_bytes;
-    if (pbuf_stream_index == pbuf_write_index) {
-        unwritten_bytes = 0;
-    } else if (pbuf_write_index > pbuf_stream_index) {
-        unwritten_bytes = pbuf_write_index - pbuf_stream_index;
-    } else {
-        // The write index has looped around to the start of the buffer.
-        unwritten_bytes 
-            = FILE_BUFFER_SIZE - (pbuf_stream_index - pbuf_write_index);
-    }
-    if (unwritten_bytes > FILE_BUFFER_SIZE / 4) {
-        printf("[STREAM] WARNING: Having troubles keeping up!\n");
-    }
-
-    while (unwritten_bytes > sample_interval) {
-        int result 
-            = write(client_fd, &primary_file_buffer[pbuf_stream_index], FRAME_SIZE);
-        if (result < 0) {
-            // We failed to write, the client has probably disconnected.
-            return 0;
-        }
-        pbuf_stream_index 
-            = (pbuf_stream_index + sample_interval) % FILE_BUFFER_SIZE;
-        unwritten_bytes -= sample_interval;
-    }
-
-    return 1; // We are good to continue.
-}
-
-void *realtime_stream_worker(void *args) {
-    int socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (socket_fd < 0) {
-        printf("[STREAM] Failed to create socket.\n");
-        exit(1);
-    }
-
-    struct sockaddr_un addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, IPC_STREAM_ID, sizeof(addr.sun_path) - 1);
-    unlink(IPC_STREAM_ID);
-    int result = bind(socket_fd, (struct sockaddr*) &addr, sizeof(addr));
-    if (result < 0) {
-        printf("[STREAM] Failed to bind socket, error code %i.\n", errno);
-        exit(1);
-    }
-    
-    if (listen(socket_fd, 5) < 0) {
-        printf("[STREAM] Failed to listen(), error code %i.\n", errno);
-        exit(1);
-    }
-
-    printf("[STREAM] Setup complete.\n");
-
-    while (continue_flag) {
-        struct sockaddr_un client_addr;
-        int addr_len = sizeof(client_addr);
-        printf("[STREAM] Waiting for connection.\n");
-        int client_fd = accept(
-            socket_fd, 
-            (struct sockaddr*) 
-            &client_addr, 
-            &addr_len
-        );
-        if (client_fd < 0) {
-            printf("[STREAM] Bad connection attempt, error code %i.\n", errno);
-            continue;
-        }
-        printf("[STREAM] Accepted an incoming connection.\n");
-        // Skip to the most currently written frame.
-        pbuf_stream_index = pbuf_write_index;
-        pbuf_stream_index -= pbuf_stream_index % FRAME_SIZE;
-        while (continue_flag && send_unstreamed_data(client_fd)) {
-            usleep(1000);
-        }
-        close(client_fd);
-    }
-
-    if (close(socket_fd) < 0) {
-        printf("[STREAM] Unable to close socket, error code %i.\n", errno);
-        return NULL;
-    } 
-    if (unlink(IPC_STREAM_ID) < 0) {
-        printf("[STREAM] Unable to unlink socket, error code %i.\n", errno);
-        return NULL;
-    } 
-    printf("[STREAM] Successfully cleaned up socket.\n");
 }
 
 int main() {
--- sensor-worker/workers.h
+++ sensor-worker/workers.h
@@ -1,10 +1,15 @@
 #ifndef WORKERS_H_
 #define WORKERS_H_
 
+#include "config.h"
+
 extern volatile int continue_flag;
 extern volatile int write_to_file_flag;
 // Only stream every (var)th frame.
 extern volatile int stream_frame_interval;
+
+extern volatile int pbuf_write_index;
+extern volatile char primary_file_buffer[FILE_BUFFER_SIZE];
 
 void *file_worker(void *args);
 void *sensor_read_worker(void *args);
